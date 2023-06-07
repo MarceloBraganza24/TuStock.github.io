8d2b58de6f9400633abe6f3059d45c4d64ac77eb
litocode24 <marcelo_braganza@hotmail.com>
2023-06-07 18:45:10 -0300

    TuStock_V1.1

--- js/main.js
+++ js/main.js
@@ -1,46 +1,84 @@
-// Arrays
 
-const codigos = [1,2,3,4,5,6,7,8,9,10]
+// variables
 
-const dniClientes = [38917705,17935299,21752752]
-
-const nombreClientes = ["Marcelo Braganza","Osvaldo Braganza","Mariela Piro",]
-
-// Objetos
-
-const prod1 = { codigo: 1, descripcion: "Sprite", precio: 100, stock: 2 }
-const prod2 = { codigo: 2, descripcion: "Baggio", precio: 740, stock: 5 }
-const prod3 = { codigo: 3, descripcion: "Turron", precio: 500, stock: 4 }
-const prod4 = { codigo: 4, descripcion: "Block", precio: 900, stock: 15 }
-const prod5 = { codigo: 5, descripcion: "Vizzio", precio: 450, stock: 9 }
-
-// Variables
-
+//botones
 const btnNuevaVenta = document.querySelector("#btnNuevaVenta")
+
 const btnProductos = document.querySelector("#btnProductos")
+
 const btnBorrarProd = document.querySelector("#btnBorrarProd")
+
 const btnConfirmarVenta = document.querySelector("#btnConfirmarVenta")
+
 const btnContinuarVendiendoCo = document.querySelector("#btnContinuarVendiendoCo")
 const btnContinuarVendiendoCa = document.querySelector("#btnContinuarVendiendoCa")
 const btnContinuarVendiendoDn = document.querySelector("#btnContinuarVendiendoDn")
+const btnContinuarVendiendoCV = document.querySelector("#btnContinuarVendiendoCV")
 
-let inputCodigoProducto = document.querySelector("#inputCodigoProducto")
-let inputDescripcion = document.querySelector("#inputDescripcion")
-let inputCantidad = document.querySelector("#inputCantidad")
-let inputPrecio = document.querySelector("#inputPrecio")
-let inputStockDisp = document.querySelector("#inputStockDisp")
-let inputDni = document.querySelector("#inputDni")
-let inputNombre = document.querySelector("#inputNombre")
+//inputs
+const inputCodigoProducto = document.querySelector("#inputCodigoProducto")
+const inputDescripcion = document.querySelector("#inputDescripcion")
+const inputCantidad = document.querySelector("#inputCantidad")
+const inputPrecio = document.querySelector("#inputPrecio")
+const inputStockDisp = document.querySelector("#inputStockDisp")
+const inputDni = document.querySelector("#inputDni")
+const inputNombre = document.querySelector("#inputNombre")
+const inputBuscadorProductosNuevaVenta = document.querySelector("#inputBuscadorProductosNuevaVenta")
 
+//selects
 let selectCantCuotas = document.querySelector("#selectCantCuotas")
 
-let popUpConfirmacionVenta = document.querySelector("#popUpConfirmacionVenta")
+//popUps
+const popUpConfirmacionVenta = document.querySelector("#popUpConfirmacionVenta")
+const popUpIngreseUnCodigo = document.querySelector("#popUpIngreseUnCodigo")
+const popUpIngreseCantidad = document.querySelector("#popUpIngreseCantidad")
+const popUpIngreseDni = document.querySelector("#popUpIngreseDni")
 
-let popUpIngreseUnCodigo = document.querySelector("#popUpIngreseUnCodigo")
-let popUpIngreseCantidad = document.querySelector("#popUpIngreseCantidad")
-let popUpIngreseDni = document.querySelector("#popUpIngreseDni")
-
+//tabla
 const tablaProductosIngresados = document.querySelector("#tablaProductosIngresados")
+
+// Arrays
+
+const listaProductosIngresadosNuevaVenta = []
+
+const listaProductos = []
+
+const listaVentas = []
+
+const descripcionProducto = ["Vizzio","Aceite","Sal","Arroz","Café","Vino Blanco","Fideos","Harina","Mayonesa","Mermelada","Vinagre","Pañales","Vino Tinto"]
+
+const productos = [
+    { codigo: 12, descripcion: "Vino Tinto", precio: 3250, stock: 15, proveedor: "Dist. chile" },
+    { codigo: 1, descripcion: "Aceite", precio: 450, stock: 2, proveedor: "Tregar" },
+    { codigo: 2, descripcion: "Arroz", precio: 300, stock: 5, proveedor: "Sancor" },
+    { codigo: 3, descripcion: "Café", precio: 500, stock: 4, proveedor: "El colo" },
+    { codigo: 10, descripcion: "Sal", precio: 740, stock: 5, proveedor: "Sancor" },
+    { codigo: 4, descripcion: "Fideos", precio: 400, stock: 15, proveedor: "Dist. chile" },
+    { codigo: 5, descripcion: "Harina", precio: 300, stock: 2, proveedor: "Tregar" },
+    { codigo: 6, descripcion: "Mayonesa", precio: 560, stock: 5, proveedor: "Sancor" },
+    { codigo: 7, descripcion: "Mermelada", precio: 750, stock: 4, proveedor: "El colo" },
+    { codigo: 8, descripcion: "Vinagre", precio: 900, stock: 15, proveedor: "Dist. chile" },
+    { codigo: 9, descripcion: "Pañales", precio: 2500, stock: 2, proveedor: "Tregar" },
+    { codigo: 11, descripcion: "Vino Blanco", precio: 3200, stock: 4, proveedor: "El colo" },
+    { codigo: 13, descripcion: "Vizzio", precio: 600, stock: 9, proveedor: "El bordo" }
+]
+
+const clientes = [
+    {nombre: "Marcelo Braganza", dni: 38917705, telefono: 2926450496},
+    {nombre: "Osvaldo Braganza", dni: 17935299, telefono: 2926452737},
+    {nombre: "Mariela Piro", dni: 21296752, telefono: 2926450156},
+]
+
+
+// Objetos
+
+function Producto (codigo,descripcion,precio,stock,proveedor) {
+    this.codigo = codigo; 
+    this.descripcion = descripcion; 
+    this.precio = precio; 
+    this.stock = stock; 
+    this.proveedor = proveedor; 
+}
 
 // Eventos
 
@@ -52,103 +90,11 @@ btnConfirmarVenta.addEventListener("click",confirmarVenta)
 btnContinuarVendiendoCo.addEventListener("click",cerrarPopUpCo)
 btnContinuarVendiendoCa.addEventListener("click",cerrarPopUpCa)
 btnContinuarVendiendoDn.addEventListener("click",cerrarPopUpDn)
+btnContinuarVendiendoCV.addEventListener("click",cerrarPopUpConfVenta)
 
-inputCodigoProducto.addEventListener("keydown",(e) => {
+inputCodigoProducto.addEventListener("keydown",setearProductoNuevaVenta)
 
-    if(e.keyCode === 13) {
-
-        const codigoProd = inputCodigoProducto.value
-        
-        if(codigoProd === ``) {
-
-            popUpIngreseUnCodigo.style.display = 'flex'
-            inputCodigoProducto.onfocus()
-
-        } else if(codigoProd === `${prod1.codigo}`) {
-
-            inputDescripcion.innerText = `${prod1.descripcion}`
-            inputPrecio.innerText = `${prod1.precio}`
-            inputStockDisp.innerText = `${prod1.stock}`
-
-        } else if(codigoProd === `${prod2.codigo}`) {
-
-            inputDescripcion.innerText = `${prod2.descripcion}`
-            inputPrecio.innerText = `${prod2.precio}`
-            inputStockDisp.innerText = `${prod2.stock}`
-
-        } else if(codigoProd === `${prod3.codigo}`) {
-
-            inputDescripcion.innerText = `${prod3.descripcion}`
-            inputPrecio.innerText = `${prod3.precio}`
-            inputStockDisp.innerText = `${prod3.stock}`
-
-        } else if(codigoProd === `${prod4.codigo}`) {
-
-            inputDescripcion.innerText = `${prod4.descripcion}`
-            inputPrecio.innerText = `${prod4.precio}`
-            inputStockDisp.innerText = `${prod4.stock}`
-
-        } else if(codigoProd === `${prod5.codigo}`) {
-
-            inputDescripcion.innerText = `${prod5.descripcion}`
-            inputPrecio.innerText = `${prod5.precio}`
-            inputStockDisp.innerText = `${prod5.stock}`
-
-        }
-
-        inputCantidad.focus()   
-
-    }
-
-})
-
-inputDni.addEventListener("keydown",(e) => {
-
-    if(e.keyCode === 13) {
-
-        const dni = inputDni.value
-        
-        if(inputDni.value === ``) {
-
-            popUpIngreseDni.style.display = 'flex'
-
-        } else if(dni === `${dniClientes[0]}`) {
-
-            inputNombre.innerText = `${nombreClientes[0]}`
-
-        } 
-
-    }
-
-})
-
-inputCantidad.addEventListener("keydown",(e) => {
-
-    const cant = inputCantidad.value
-    
-    if(e.keyCode === 13) {
-
-        if(cant === ``) {
-
-            popUpIngreseCantidad.style.display = 'flex' 
-    
-        } else {
-            let div = document.createElement("div")
-
-            div.className = "bodyIndex__ventanas__nuevaVenta__divTabla__tabla__productoIngresado"
-
-            div.innerHTML = `<div class="bodyIndex__ventanas__nuevaVenta__divTabla__tabla__productoIngresado__bor">${inputCodigoProducto.value}</div> <div class="bodyIndex__ventanas__nuevaVenta__divTabla__tabla__productoIngresado__bor">${inputDescripcion.innerText}</div> <div class="bodyIndex__ventanas__nuevaVenta__divTabla__tabla__productoIngresado__bor">${inputCantidad.value}</div> <div class="bodyIndex__ventanas__nuevaVenta__divTabla__tabla__productoIngresado__bor">${inputPrecio.innerText}</div> <div>${inputPrecio.innerText * inputCantidad.value}</div>`
-
-            tablaProductosIngresados.appendChild(div)
-
-            limpiarImputsNuevaVenta()
-            inputCodigoProducto.focus()
-
-        }
-
-    }
-
-})
+inputCantidad.addEventListener("keydown",setearProductoListaNuevaVenta)
 
 // Funciones
 
@@ -184,7 +130,18 @@ function limpiarImputsNuevaVenta() {
 }
 
 function confirmarVenta() {
-    popUpConfirmacionVenta.style.display = 'flex'
+
+    if (inputDni.value === '') {
+        popUpIngreseDni.style.display = 'flex'
+    } else {
+        popUpConfirmacionVenta.style.display = 'flex'
+    }
+
+}
+
+function cerrarPopUpConfVenta() {
+    popUpConfirmacionVenta.style.display = 'none'
+    mostrarVentanaNuevaVenta()
 }
 
 function cerrarPopUpCo() {
@@ -202,7 +159,97 @@ function cerrarPopUpDn() {
     inputDni.focus()
 }
 
+function mostrarListaProductos() {
+
+    for (producto of productos) {
+
+        let div = document.createElement("div")
+        div.className = "bodyIndex__ventanas__productos__divTablaVentanaProd__tabla__tablaListaProductos__producto"
+        div.innerHTML = 
+        `
+        <div class="bodyIndex__ventanas__productos__divTablaVentanaProd__tabla__tablaListaProductos__producto__bor">${producto.codigo}</div>
+        <div class="bodyIndex__ventanas__productos__divTablaVentanaProd__tabla__tablaListaProductos__producto__bor">${producto.descripcion}</div>
+        <div class="bodyIndex__ventanas__productos__divTablaVentanaProd__tabla__tablaListaProductos__producto__bor">${producto.precio}</div>
+        <div class="bodyIndex__ventanas__productos__divTablaVentanaProd__tabla__tablaListaProductos__producto__bor">${producto.stock}</div>
+        <div>${producto.proveedor}</div>
+        `;
+        tablaListaProductos.appendChild(div)
+
+    }
+
+}
+
+function setearProductoNuevaVenta(e) {
+
+    if(e.keyCode === 13) {
+
+        for (producto of productos) {
+    
+            if(inputCodigoProducto.value === ``) {
+    
+                popUpIngreseUnCodigo.style.display = 'flex' 
+        
+            } else if (inputCodigoProducto.value == producto.codigo) {
+                inputDescripcion.innerHTML = `<div>${producto.descripcion}</div>`
+                inputPrecio.innerHTML = `<div>${producto.precio}</div>`
+                inputStockDisp.innerHTML = `<div>${producto.stock}</div>`
+                inputCantidad.focus()
+            }
+    
+        }
+    
+    }
+
+}
+
+function setearProductoListaNuevaVenta(e) {
+
+    const cant = inputCantidad.value
+    const codigo = inputCodigoProducto.value
+    
+    if(e.keyCode === 13) {
+
+        if(cant === ``) {
+
+            popUpIngreseCantidad.style.display = 'flex' 
+    
+        }  else {
+            let div = document.createElement("div")
+
+            div.className = "bodyIndex__ventanas__nuevaVenta__divTablaNuevaVenta__tabla__productoIngresado"
+
+            div.innerHTML = `<div id="codProdIngresado" class="bodyIndex__ventanas__nuevaVenta__divTablaNuevaVenta__tabla__productoIngresado__bor">${inputCodigoProducto.value}</div> <div class="bodyIndex__ventanas__nuevaVenta__divTablaNuevaVenta__tabla__productoIngresado__bor">${inputDescripcion.innerText}</div> <div class="bodyIndex__ventanas__nuevaVenta__divTablaNuevaVenta__tabla__productoIngresado__bor">${inputCantidad.value}</div> <div class="bodyIndex__ventanas__nuevaVenta__divTablaNuevaVenta__tabla__productoIngresado__bor">${inputPrecio.innerText}</div> <div>${inputPrecio.innerText * inputCantidad.value}</div>`
+
+            tablaProductosIngresados.appendChild(div)
+            limpiarImputsNuevaVenta()
+            inputCodigoProducto.focus()
+
+            for (producto of productos) {
+                if(codigo == producto.codigo) {
+                    producto.stock = producto.stock - cant
+                }
+            }
+
+        }
+
+    }
+
+}
+
 // Llamada a las funciones
 
 mostrarVentanaNuevaVenta()
 
+mostrarListaProductos()
+
+
+
+
+// Pruebas
+
+
+
+
+
+
+// Fin pruebas
